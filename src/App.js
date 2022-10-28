885ccb59bfa4d9174474877001545a6e5641732c
danutibe07 <danielutibe07@gmail.com>
2022-10-28 18:13:53 +0100

    subtext to link

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import image7 from "../src/images/slack.svg"
 import image8 from "../src/images/git.svg"
 import image9 from "../src/images/more.svg"
 import image10 from "../src/images/img2.svg"
+import image11 from "../src/images/more2.svg"
 function App() {
   return (
     <div className="App">
@@ -16,6 +17,7 @@ function App() {
       <img className="sharebtn" src={image2} alt="image2"></img> 
       <img className="sharebtnn" src={image3} alt="image3"></img>
       <img className="morebtn" src={image9} alt="image9"></img>
+      <img className="morebtnn" src={image11} alt="image11"></img>
     </div>
     <img src={image1} alt="image1" className="profileimage1" id="profile__img"></img>
     <img src={image10} alt="image10" className="profileimage2" id="profile__img"></img>
@@ -25,10 +27,10 @@ function App() {
       <div className="linkstrees">
         <a href="https://twitter.com/Danielutibe07?t=2kvKPTZQ7IGCw2FugE9xCQ&s=09" id="twitter">Twitter Link</a>
         <a href="https://training.zuri.team/" id="btn__zuri">Zuri Team</a>
-        <a href="https://books.zuri.team/" id="books">Zuri Books</a>
-        <a href="https://books.zuri.team/python-for-beginners?ref_id=daniel07" id="book__python">python Books</a>
-        <a href="https://background.zuri.team/" id="pitch">Background Check for Coders</a>
-        <a href="https://books.zuri.team/design-rules" id="book__design">Design Books</a>
+        <a href="https://books.zuri.team/" id="books" title="books about design and coding">Zuri Books</a>
+        <a href="https://books.zuri.team/python-for-beginners?ref_id=daniel07" id="book__python" title="you can also checkout this my python book">Python Books</a>
+        <a href="https://background.zuri.team/" id="pitch" title="can do background checks for your applicants">Background Check for Coders</a>
+        <a href="https://books.zuri.team/design-rules" id="book__design" title="free design book offered by Zuri">Design Books</a>
       </div>
       <div className="socialIcons">
         <img src={image7} alt="image7" id="slackicon"></img>
