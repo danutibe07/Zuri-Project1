a03666b660cc2fa59b6b187938adfcea576c3dac
danutibe07 <danielutibe07@gmail.com>
2022-11-04 23:47:34 +0100

    css: styling

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -20,15 +20,6 @@ const Contact = () => {
     // console.log(inputField);
   };
 
-  // const handleSubmit = (e) => {
-  //   e.preventDefault();
-  //   setInputErrors(validate(inputField));
-  //   setIsSubmit(true);
-  //   if (Object.keys(inputErrors).length === 0 && isSubmit) {
-  //     alert("submit")
-  //   }
-  // };
-
   const handleSubmit = (e) => {
     e.preventDefault();
     setInputErrors(validate(inputField));
@@ -36,14 +27,14 @@ const Contact = () => {
 
     if (Object.keys(inputErrors).length === 0 && isSubmit) {
       alert("mssg sent")
-      .then(() =>
-        setInputField({
-          firstname: "",
-          lastname: "",
-          email: "",
-          message: "",
-        })
-      );
+        .then(() =>
+          setInputField({
+            firstname: "",
+            lastname: "",
+            email: "",
+            message: "",
+          })
+        );
     }
   };
 
@@ -68,41 +59,44 @@ const Contact = () => {
   };
 
   return (
-    <section className="contact section" id="about">
+    <div className="contactpage__container">
+      <section className="contactpage">
         <h2 className="contact__title">Contact Me</h2>
         <p className="contact__subtitle">Hi there, contact me to ask me about anything you have in mind.</p>
         <form className="contactpage__form" onSubmit={handleSubmit}>
-        <div className="first__and__last__name">
-          <div className="firstname">
-            <label htmlFor="" className="contactpage__label">First name</label>  <br />
-            <input type="text" id='first_name' className="first__name" placeholder='Enter your first name'  onChange={handleChange}
-            value={inputField.firstname}   name="firstname"/>
-             <p className="text-danger">{inputErrors.firstname}</p>
+          <div className="first__and__last__name">
+            <div className="firstname">
+              <label htmlFor="" className="contactpage__label">First name</label>  <br />
+              <input type="text" id='first_name' className="first__name" placeholder='Enter your first name' onChange={handleChange}
+                value={inputField.firstname} name="firstname" />
+              <p className="text-danger">{inputErrors.firstname}</p>
+            </div>
+            <div className="lastname">
+              <label htmlFor="" className="contactpage__label">Last name</label>  <br />
+              <input type="text" id='last_name' className="last__name" placeholder='Enter your last name' onChange={handleChange} value={inputField.lastname} name="lastname" />
+              <p className="text-danger">{inputErrors.lastname}</p>
+            </div>
           </div>
-          <div className="lastname">
-            <label htmlFor="" className="contactpage__label">Last name</label>  <br /> 
-            <input type="text" id='last_name' className="last__name" placeholder='Enter your last name'  onChange={handleChange}  value={inputField.lastname}   name="lastname"/>
-            <p className="text-danger">{inputErrors.lastname}</p>
+          <div className="emailaddress">
+            <label htmlFor="" className="contactpage__label">Email</label>  <br />
+            <input type="email" id='email' className="email" placeholder='yourname@email.com' value={inputField.email} name="email" onChange={handleChange} />
+            <p className="text-danger">{inputErrors.email}</p>
           </div>
-        </div>
-        <div className="emailaddress">
-          <label htmlFor="" className="contactpage__label">Email</label>  <br />
-          <input type="email" id='email' className="email" placeholder='yourname@email.com' value={inputField.email}   name="email"  onChange={handleChange}/>
-          <p className="text-danger">{inputErrors.email}</p>
-        </div>
-        <div className="contact__message">
-          <label htmlFor="" className="contactpage__label">Message</label>  <br />
-          <textarea name="message" type="text" className='none' id="message" cols="30" rows="10" placeholder="Send me a message and I'll reply you as soon as possible..." value={inputField.message}  onChange={handleChange} ></textarea>
-          <p className="text-danger">{inputErrors.message}</p>
-          <div className="contact__checkbox">
-            <input type="checkbox" className='checkbox_btn' required/>
-            <p>You agree to providing your data to {name} who may contact you.</p>
+          <div className="contact__message">
+            <label htmlFor="" className="contactpage__label">Message</label>  <br />
+            <textarea name="message" type="text" className='none' id="message" cols="30" rows="10" placeholder="Send me a message and I'll reply you as soon as possible..." value={inputField.message} onChange={handleChange} ></textarea>
+            <p className="text-danger">{inputErrors.message}</p>
+            <div className="contact__checkbox">
+              <input type="checkbox" className='checkbox_btn' required />
+              <p>You agree to providing your data to {name} who may contact you.</p>
+            </div>
           </div>
-        </div>
-         <button id="btn__submit" type='submit'>Send Message</button>
+          <button id="btn__submit" type='submit'>Send Message</button>
         </form>
-       
+
       </section>
+    </div>
+
   )
-} 
+}
 export default Contact;
