b596fc72a3ab4c62b2b8c5887d9a5b8b5885fda4
danutibe07 <danielutibe07@gmail.com>
2022-11-05 13:46:23 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React from 'react'
-import Home from './Components/Home/Home'
-import {  Routes , Route } from 'react-router-dom';
-import Contact from './Components/Contact/Contact'
-import Footer from './Components/Footer/Footer';
+import React from "react";
+import Home from "./Components/Home/Home";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
 const App = () => {
   return (
-    <div className='App'>
+    <div className="App">
+      <BrowserRouter basename="Zuri-Project1">
         <Routes>
           <Route exact path="/" element={<Home />} />
-          <Route exact path="/contact" element={<Contact />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
+      </BrowserRouter>
+
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
