e751bec728c792c48ae4ab7c965f5f8cbda6a497
danutibe07 <danielutibe07@gmail.com>
2022-11-05 12:43:27 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react'
 import Home from './Components/Home/Home'
-import {  Routes , BrowserRouter, Route } from 'react-router-dom';
+import {  Routes , Route } from 'react-router-dom';
 import Contact from './Components/Contact/Contact'
 import Footer from './Components/Footer/Footer';
 const App = () => {
   return (
     <div className='App'>
-      <BrowserRouter>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route exact path="/contact" element={<Contact />} />
         </Routes>
-      </BrowserRouter>
       <Footer />
     </div>
   )
